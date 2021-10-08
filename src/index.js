8b408e0c28c7d0fba51c9f189fb86bb559a05bda
Matt Moore <Matt.M.Moore13@outlook.com>
2021-10-08 17:14:21 -0400

    Added react-component-library dependencies

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { defineCustomElements } from 'mattsstenciltest'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
